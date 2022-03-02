4c03d29931fc2552c50f64e80be8101ce098c91a
asslikealex <alex_todor@yahoo.com>
2022-03-02 19:52:30 +0100

    login design 2.0

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -12,13 +12,41 @@ var particlesJS: any;
 })
 export class LoginComponent implements OnInit {
 
+    myStyle: object = {};
+    myParams: object = {};
+    width: number = 100;
+    height: number = 100;
+
   email : string = '';
   password : string = '';
 
   constructor(private auth : AuthService) { }
 
   ngOnInit(): void {
-    
+    this.myStyle = {
+      'position': 'fixed',
+      'width': '100%',
+      'height': '100%',
+      'z-index': -1,
+      'top': 0,
+      'left': 0,
+      'right': 0,
+      'bottom': 0,
+  };
+
+this.myParams = {
+      particles: {
+          number: {
+              value: 200,
+          },
+          color: {
+              value: '#ff0000'
+          },
+          shape: {
+              type: 'triangle',
+          },
+  }
+  };
   }
 
   login() {
